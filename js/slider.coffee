8fd0521ebf9243b1999df4c9a72b341e72045fa0
andrey-abramow <abramow.andrey@gmail.com>
2015-01-23 01:01:03 +0200

    fixing

--- js/slider.coffee
+++ js/slider.coffee
@@ -4,8 +4,6 @@ app.controller 'mainCtrl', [($scope)->
   $scope.maxValue = 3
 
 ]
-
-
 app.directive 'slider', ->
   restrict : 'A'
   scope :
@@ -16,44 +14,82 @@ app.directive 'slider', ->
   templateUrl : './templates/slider-template.html'
 
   link: (scope, element) ->
+    # moving max pointer
+    width = element[0].clientWidth
     minElement = document.getElementsByClassName('slider-btn min')[0]
     maxElement = document.getElementsByClassName('slider-btn max')[0]
+    maxElement.style.right = -30
+    minElement.style.left = -30
+
+
     sliderRange = document.getElementById('slider-range')
-    currentX = -31
-    sliderRangeCurrentX = 150 - 31
-    console.log sliderRange
-    onDragEvent = false
-    onDropEvent = false
+    sliderRangeCurrentXMAX = 150 - 30
+    sliderRangeCurrentXMIN = 150
+    onDragEventMIN = false
+    onDropEventMAX = false
     startPosition = 0
     finishPosition = 0
+    maxPosition =  100000
+    minPosition = -100000
 
     maxElement.addEventListener 'mousedown', (event)->
-      console.log currentX
+      maxPosition =  100000
+      minPosition = -100000
       if maxElement.style.right
-          console.log maxElement.style.right
-          currentX = maxElement.style.right.slice(0,-2)
-          sliderRangeCurrentX = sliderRange.style.right.slice(0,-2)
-      else
-        console.log '----------------------'
-#      console.log currentX
-
-      onDragEvent = true
+        sliderRangeCurrentXMAX = sliderRange.style.right.slice(0,-2)
+      onDropEventMAX = true
       startPosition = event.clientX
 
+    minElement.addEventListener 'mousedown', (event)->
+      maxPosition =  100000
+      minPosition = -100000
+      console.log 'f'
+      if minElement.style.left
+        sliderRangeCurrentXMIN = sliderRange.style.left.slice(0,-2)
+      onDragEventMIN = true
+      startPosition = event.clientX
+
+
+
     document.addEventListener 'mouseup', (event)->
-      onDragEvent = false
+      onDropEventMAX = false
+      onDragEventMIN = false
+
+
 
 
 
     document.body.onmousemove = (event)->
-      console.log 'move'
-#      console.log  onDragEvent
-      finishPosition = event.clientX
-      if onDragEvent
-        sliderRange.style.right  = sliderRangeCurrentX - (finishPosition - startPosition)
-        maxElement.style.right += finishPosition - startPosition
-        console.log startPosition, finishPosition, maxElement.style.right, currentX, sliderRange.style.right
-        scope.maxValue = maxElement.style.right
+      #for max
+      if onDropEventMAX
+        finishPosition = event.clientX
+        if (finishPosition < maxPosition) && (finishPosition > minPosition)
+          sliderRange.style.right  = sliderRangeCurrentXMAX - (finishPosition - startPosition)
+          maxElement.style.right += finishPosition - startPosition
+        if (sliderRange.style.right.slice(0,-2) < 0)
+          sliderRange.style.right = '0px'
+          maxPosition = event.clientX
+        console.log sliderRange.clientWidth
+        if sliderRange.clientWidth <= -1
+          sliderRange.style.right = (sliderRange.style.right.slice(0,-2)-1)+'px'
+          minPosition = event.clientX
+
+#     for min
+      if onDragEventMIN
+        console.log 'efef'
+        finishPosition = event.clientX
+        console.log finishPosition,maxPosition,minPosition
+        if (finishPosition < maxPosition) && (finishPosition > minPosition)
+          sliderRange.style.left  = sliderRangeCurrentXMIN - (startPosition - finishPosition)
+          minElement.style.left +=  startPosition - finishPosition
+          console.log minElement.style.left
+        if (sliderRange.style.left.slice(0,-2) < 0)
+          sliderRange.style.left = '0px'
+          maxPosition = event.clientX
+        if sliderRange.clientWidth <= -1
+          sliderRange.style.left = (sliderRange.style.left.slice(0,-2)-1)+'px'
+          minPosition = event.clientX
+
 
 
     false
--- public/js/slider.js
+++ public/js/slider.js
@@ -17,40 +17,78 @@ app.directive('slider', function() {
     },
     templateUrl: './templates/slider-template.html',
     link: function(scope, element) {
-      var currentX, finishPosition, maxElement, minElement, onDragEvent, onDropEvent, sliderRange, sliderRangeCurrentX, startPosition;
+      var finishPosition, maxElement, maxPosition, minElement, minPosition, onDragEventMIN, onDropEventMAX, sliderRange, sliderRangeCurrentXMAX, sliderRangeCurrentXMIN, startPosition, width;
+      width = element[0].clientWidth;
       minElement = document.getElementsByClassName('slider-btn min')[0];
       maxElement = document.getElementsByClassName('slider-btn max')[0];
+      maxElement.style.right = -30;
+      minElement.style.left = -30;
       sliderRange = document.getElementById('slider-range');
-      currentX = -31;
-      sliderRangeCurrentX = 150 - 31;
-      console.log(sliderRange);
-      onDragEvent = false;
-      onDropEvent = false;
+      sliderRangeCurrentXMAX = 150 - 30;
+      sliderRangeCurrentXMIN = 150;
+      onDragEventMIN = false;
+      onDropEventMAX = false;
       startPosition = 0;
       finishPosition = 0;
+      maxPosition = 100000;
+      minPosition = -100000;
       maxElement.addEventListener('mousedown', function(event) {
-        console.log(currentX);
+        maxPosition = 100000;
+        minPosition = -100000;
         if (maxElement.style.right) {
-          console.log(maxElement.style.right);
-          currentX = maxElement.style.right.slice(0, -2);
-          sliderRangeCurrentX = sliderRange.style.right.slice(0, -2);
-        } else {
-          console.log('----------------------');
+          sliderRangeCurrentXMAX = sliderRange.style.right.slice(0, -2);
         }
-        onDragEvent = true;
+        onDropEventMAX = true;
+        return startPosition = event.clientX;
+      });
+      minElement.addEventListener('mousedown', function(event) {
+        maxPosition = 100000;
+        minPosition = -100000;
+        console.log('f');
+        if (minElement.style.left) {
+          sliderRangeCurrentXMIN = sliderRange.style.left.slice(0, -2);
+        }
+        onDragEventMIN = true;
         return startPosition = event.clientX;
       });
       document.addEventListener('mouseup', function(event) {
-        return onDragEvent = false;
+        onDropEventMAX = false;
+        return onDragEventMIN = false;
       });
       document.body.onmousemove = function(event) {
-        console.log('move');
-        finishPosition = event.clientX;
-        if (onDragEvent) {
-          sliderRange.style.right = sliderRangeCurrentX - (finishPosition - startPosition);
-          maxElement.style.right += finishPosition - startPosition;
-          console.log(startPosition, finishPosition, maxElement.style.right, currentX, sliderRange.style.right);
-          return scope.maxValue = maxElement.style.right;
+        if (onDropEventMAX) {
+          finishPosition = event.clientX;
+          if ((finishPosition < maxPosition) && (finishPosition > minPosition)) {
+            sliderRange.style.right = sliderRangeCurrentXMAX - (finishPosition - startPosition);
+            maxElement.style.right += finishPosition - startPosition;
+          }
+          if (sliderRange.style.right.slice(0, -2) < 0) {
+            sliderRange.style.right = '0px';
+            maxPosition = event.clientX;
+          }
+          console.log(sliderRange.clientWidth);
+          if (sliderRange.clientWidth <= -1) {
+            sliderRange.style.right = (sliderRange.style.right.slice(0, -2) - 1) + 'px';
+            minPosition = event.clientX;
+          }
+        }
+        if (onDragEventMIN) {
+          console.log('efef');
+          finishPosition = event.clientX;
+          console.log(finishPosition, maxPosition, minPosition);
+          if ((finishPosition < maxPosition) && (finishPosition > minPosition)) {
+            sliderRange.style.left = sliderRangeCurrentXMIN - (startPosition - finishPosition);
+            minElement.style.left += startPosition - finishPosition;
+            console.log(minElement.style.left);
+          }
+          if (sliderRange.style.left.slice(0, -2) < 0) {
+            sliderRange.style.left = '0px';
+            maxPosition = event.clientX;
+          }
+          if (sliderRange.clientWidth <= -1) {
+            sliderRange.style.left = (sliderRange.style.left.slice(0, -2) - 1) + 'px';
+            return minPosition = event.clientX;
+          }
         }
       };
       return false;
