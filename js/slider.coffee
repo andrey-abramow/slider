5bc4394381ef653b4831b7db345c399ab13fa6b9
andrey <andrey@rademade.com>
2015-01-23 12:57:38 +0200

    finxing bugs

--- js/slider.coffee
+++ js/slider.coffee
@@ -1,7 +1,9 @@
-window.app = angular.module('app', [])
-app.controller 'mainCtrl', [($scope)->
-  $scope.minimum = 4
-  $scope.maxValue = 3
+window.app = angular.module('app', ['ngTouch'])
+app.controller 'mainCtrl', ['$scope', ($scope)->
+  $scope.minimum = 14
+  $scope.maximum = 65
+  $scope.$watch 'minimum', ->
+    console.log $scope.minimum
 
 ]
 app.directive 'slider', ->
@@ -14,15 +16,27 @@ app.directive 'slider', ->
   templateUrl : './templates/slider-template.html'
 
   link: (scope, element) ->
+    console.log scope
     # moving max pointer
     width = element[0].clientWidth
     minElement = document.getElementsByClassName('slider-btn min')[0]
     maxElement = document.getElementsByClassName('slider-btn max')[0]
+    sliderContainer = document.getElementsByClassName('slider-container')[0]
+    sliderRange = document.getElementById('slider-range')
+
+
+    maxWidthRange = sliderContainer.clientWidth
     maxElement.style.right = -30
     minElement.style.left = -30
+    #set init value
+    sliderRange.style.left = 0
+    sliderRange.style.right = 0
+    step = sliderRange.clientWidth / (scope.maxValue - scope.minValue)
+    console.log step
 
+    initMaxValue = scope.maxValue
+    initMinValue = scope.minValue
 
-    sliderRange = document.getElementById('slider-range')
     sliderRangeCurrentXMAX = 150 - 30
     sliderRangeCurrentXMIN = 150
     onDragEventMIN = false
@@ -43,12 +57,10 @@ app.directive 'slider', ->
     minElement.addEventListener 'mousedown', (event)->
       maxPosition =  100000
       minPosition = -100000
-      console.log 'f'
       if minElement.style.left
         sliderRangeCurrentXMIN = sliderRange.style.left.slice(0,-2)
       onDragEventMIN = true
       startPosition = event.clientX
-
 
 
     document.addEventListener 'mouseup', (event)->
@@ -56,40 +68,47 @@ app.directive 'slider', ->
       onDragEventMIN = false
 
 
-
-
-
     document.body.onmousemove = (event)->
       #for max
       if onDropEventMAX
         finishPosition = event.clientX
+
         if (finishPosition < maxPosition) && (finishPosition > minPosition)
-          sliderRange.style.right  = sliderRangeCurrentXMAX - (finishPosition - startPosition)
-          maxElement.style.right += finishPosition - startPosition
+          sliderRange.style.right = sliderRangeCurrentXMAX - (finishPosition - startPosition)
+          setMaxValue()
+#          scope.maxValue = initMaxValue - Math.floor sliderRange.style.right.slice(0,-2)/step if Math.floor sliderRange.style.right.slice(0,-2) > 0
+#          scope.maxValue = scope.minValue + 1 if scope.maxValue <= scope.minValue
+          scope.$apply()
         if (sliderRange.style.right.slice(0,-2) < 0)
           sliderRange.style.right = '0px'
           maxPosition = event.clientX
-        console.log sliderRange.clientWidth
-        if sliderRange.clientWidth <= -1
-          sliderRange.style.right = (sliderRange.style.right.slice(0,-2)-1)+'px'
+        if (maxWidthRange < ((1*sliderRange.style.left.slice(0,-2) + 1*sliderRange.style.right.slice(0,-2) + sliderRange.clientWidth))) && (finishPosition < maxPosition)
           minPosition = event.clientX
+          sliderRange.style.right = maxWidthRange - sliderRange.style.left.slice(0,-2)
 
-#     for min
+      #     for min
       if onDragEventMIN
-        console.log 'efef'
         finishPosition = event.clientX
-        console.log finishPosition,maxPosition,minPosition
         if (finishPosition < maxPosition) && (finishPosition > minPosition)
-          sliderRange.style.left  = sliderRangeCurrentXMIN - (startPosition - finishPosition)
-          minElement.style.left +=  startPosition - finishPosition
-          console.log minElement.style.left
+          sliderRange.style.left = sliderRangeCurrentXMIN - (startPosition - finishPosition)
+          setMinValue()
+#          scope.minValue = initMinValue + Math.floor sliderRange.style.left.slice(0,-2)/step if sliderRange.style.left.slice(0,-2) > 0
+#          scope.minValue = scope.maxValue - 1  if scope.maxValue  <= scope.minValue
+          scope.$apply()
         if (sliderRange.style.left.slice(0,-2) < 0)
           sliderRange.style.left = '0px'
-          maxPosition = event.clientX
-        if sliderRange.clientWidth <= -1
-          sliderRange.style.left = (sliderRange.style.left.slice(0,-2)-1)+'px'
           minPosition = event.clientX
+        if (maxWidthRange < ((1*sliderRange.style.left.slice(0,-2) + 1*sliderRange.style.right.slice(0,-2) + sliderRange.clientWidth))) && (finishPosition < maxPosition)
+          maxPosition = event.clientX
+          sliderRange.style.left = maxWidthRange - sliderRange.style.right.slice(0,-2)
 
+
+    setMaxValue = ()->
+      scope.maxValue = initMaxValue - Math.floor sliderRange.style.right.slice(0,-2)/step if Math.floor sliderRange.style.right.slice(0,-2) > 0
+      scope.maxValue = scope.minValue + 1 if scope.maxValue <= scope.minValue
+    setMinValue = ()->
+      scope.minValue = initMinValue + Math.floor sliderRange.style.left.slice(0,-2)/step if sliderRange.style.left.slice(0,-2) > 0
+      scope.minValue = scope.maxValue - 1  if scope.maxValue  <= scope.minValue
 
 
     false
--- public/js/slider.js
+++ public/js/slider.js
@@ -1,9 +1,12 @@
-window.app = angular.module('app', []);
+window.app = angular.module('app', ['ngTouch']);
 
 app.controller('mainCtrl', [
-  function($scope) {
-    $scope.minimum = 4;
-    return $scope.maxValue = 3;
+  '$scope', function($scope) {
+    $scope.minimum = 14;
+    $scope.maximum = 65;
+    return $scope.$watch('minimum', function() {
+      return console.log($scope.minimum);
+    });
   }
 ]);
 
@@ -17,13 +20,22 @@ app.directive('slider', function() {
     },
     templateUrl: './templates/slider-template.html',
     link: function(scope, element) {
-      var finishPosition, maxElement, maxPosition, minElement, minPosition, onDragEventMIN, onDropEventMAX, sliderRange, sliderRangeCurrentXMAX, sliderRangeCurrentXMIN, startPosition, width;
+      var finishPosition, initMaxValue, initMinValue, maxElement, maxPosition, maxWidthRange, minElement, minPosition, onDragEventMIN, onDropEventMAX, setMaxValue, setMinValue, sliderContainer, sliderRange, sliderRangeCurrentXMAX, sliderRangeCurrentXMIN, startPosition, step, width;
+      console.log(scope);
       width = element[0].clientWidth;
       minElement = document.getElementsByClassName('slider-btn min')[0];
       maxElement = document.getElementsByClassName('slider-btn max')[0];
+      sliderContainer = document.getElementsByClassName('slider-container')[0];
+      sliderRange = document.getElementById('slider-range');
+      maxWidthRange = sliderContainer.clientWidth;
       maxElement.style.right = -30;
       minElement.style.left = -30;
-      sliderRange = document.getElementById('slider-range');
+      sliderRange.style.left = 0;
+      sliderRange.style.right = 0;
+      step = sliderRange.clientWidth / (scope.maxValue - scope.minValue);
+      console.log(step);
+      initMaxValue = scope.maxValue;
+      initMinValue = scope.minValue;
       sliderRangeCurrentXMAX = 150 - 30;
       sliderRangeCurrentXMIN = 150;
       onDragEventMIN = false;
@@ -44,7 +56,6 @@ app.directive('slider', function() {
       minElement.addEventListener('mousedown', function(event) {
         maxPosition = 100000;
         minPosition = -100000;
-        console.log('f');
         if (minElement.style.left) {
           sliderRangeCurrentXMIN = sliderRange.style.left.slice(0, -2);
         }
@@ -60,35 +71,49 @@ app.directive('slider', function() {
           finishPosition = event.clientX;
           if ((finishPosition < maxPosition) && (finishPosition > minPosition)) {
             sliderRange.style.right = sliderRangeCurrentXMAX - (finishPosition - startPosition);
-            maxElement.style.right += finishPosition - startPosition;
+            setMaxValue();
+            scope.$apply();
           }
           if (sliderRange.style.right.slice(0, -2) < 0) {
             sliderRange.style.right = '0px';
             maxPosition = event.clientX;
           }
-          console.log(sliderRange.clientWidth);
-          if (sliderRange.clientWidth <= -1) {
-            sliderRange.style.right = (sliderRange.style.right.slice(0, -2) - 1) + 'px';
+          if ((maxWidthRange < (1 * sliderRange.style.left.slice(0, -2) + 1 * sliderRange.style.right.slice(0, -2) + sliderRange.clientWidth)) && (finishPosition < maxPosition)) {
             minPosition = event.clientX;
+            sliderRange.style.right = maxWidthRange - sliderRange.style.left.slice(0, -2);
           }
         }
         if (onDragEventMIN) {
-          console.log('efef');
           finishPosition = event.clientX;
-          console.log(finishPosition, maxPosition, minPosition);
           if ((finishPosition < maxPosition) && (finishPosition > minPosition)) {
             sliderRange.style.left = sliderRangeCurrentXMIN - (startPosition - finishPosition);
-            minElement.style.left += startPosition - finishPosition;
-            console.log(minElement.style.left);
+            setMinValue();
+            scope.$apply();
           }
           if (sliderRange.style.left.slice(0, -2) < 0) {
             sliderRange.style.left = '0px';
+            minPosition = event.clientX;
+          }
+          if ((maxWidthRange < (1 * sliderRange.style.left.slice(0, -2) + 1 * sliderRange.style.right.slice(0, -2) + sliderRange.clientWidth)) && (finishPosition < maxPosition)) {
             maxPosition = event.clientX;
+            return sliderRange.style.left = maxWidthRange - sliderRange.style.right.slice(0, -2);
           }
-          if (sliderRange.clientWidth <= -1) {
-            sliderRange.style.left = (sliderRange.style.left.slice(0, -2) - 1) + 'px';
-            return minPosition = event.clientX;
-          }
+        }
+      };
+      setMaxValue = function() {
+        if (Math.floor(sliderRange.style.right.slice(0, -2) > 0)) {
+          scope.maxValue = initMaxValue - Math.floor(sliderRange.style.right.slice(0, -2) / step);
+        }
+        if (scope.maxValue <= scope.minValue) {
+          return scope.maxValue = scope.minValue + 1;
+        }
+      };
+      setMinValue = function() {
+        if (sliderRange.style.left.slice(0, -2) > 0) {
+          scope.minValue = initMinValue + Math.floor(sliderRange.style.left.slice(0, -2) / step);
+        }
+        if (scope.maxValue <= scope.minValue) {
+          return scope.minValue = scope.maxValue - 1;
         }
       };
       return false;
